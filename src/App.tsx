b73ea232c9d13b589302d55cc6f6cb32641aa7d4
hagerSe <agerneshdereje8@gmail.com>
2026-07-20 02:21:14 -1000

    update the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 
-import React from 'react'
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Component/Navbar'
 import Footer from './Component/Footer'
--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -1,7 +1,7 @@
 // src/components/Footer.tsx
 
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 
 const Footer: React.FC = () => {
   return (
